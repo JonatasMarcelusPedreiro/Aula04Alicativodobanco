8e28d420d5894dc90df4560902ffb7a82f236b40
Jônatas Marcelus P <135742926+JonatasMarcelusPedreiro@users.noreply.github.com>
2023-07-13 17:16:34 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Aula04AppBanco
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblOperacao = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.btnDepositar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnExtrato = new System.Windows.Forms.Button();
+            this.lblSaldo = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnSalvar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalvar.Location = new System.Drawing.Point(109, 212);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(145, 40);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtValor);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.lblOperacao);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnSacar);
+            this.panel1.Controls.Add(this.btnDepositar);
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Location = new System.Drawing.Point(47, 172);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 266);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(73, 186);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(228, 20);
+            this.txtValor.TabIndex = 8;
+            this.txtValor.TextChanged += new System.EventHandler(this.txtValor_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(138, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "informe o valor:";
+            // 
+            // lblOperacao
+            // 
+            this.lblOperacao.AutoSize = true;
+            this.lblOperacao.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperacao.Location = new System.Drawing.Point(136, 127);
+            this.lblOperacao.Name = "lblOperacao";
+            this.lblOperacao.Size = new System.Drawing.Size(98, 27);
+            this.lblOperacao.TabIndex = 6;
+            this.lblOperacao.Text = "Depósito";
+            this.lblOperacao.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Operações disponíveis:";
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnSacar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSacar.Location = new System.Drawing.Point(210, 55);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(134, 49);
+            this.btnSacar.TabIndex = 2;
+            this.btnSacar.Text = "Sacar";
+            this.btnSacar.UseVisualStyleBackColor = false;
+            this.btnSacar.Click += new System.EventHandler(this.btnSacar_Click);
+            // 
+            // btnDepositar
+            // 
+            this.btnDepositar.BackColor = System.Drawing.SystemColors.GrayText;
+            this.btnDepositar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDepositar.Location = new System.Drawing.Point(42, 55);
+            this.btnDepositar.Name = "btnDepositar";
+            this.btnDepositar.Size = new System.Drawing.Size(128, 49);
+            this.btnDepositar.TabIndex = 1;
+            this.btnDepositar.Text = "Depositar";
+            this.btnDepositar.UseVisualStyleBackColor = false;
+            this.btnDepositar.Click += new System.EventHandler(this.btnDepositar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(150, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Center Bank";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Seu saldo atual:";
+            // 
+            // btnExtrato
+            // 
+            this.btnExtrato.BackColor = System.Drawing.SystemColors.Desktop;
+            this.btnExtrato.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExtrato.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExtrato.Location = new System.Drawing.Point(281, 100);
+            this.btnExtrato.Name = "btnExtrato";
+            this.btnExtrato.Size = new System.Drawing.Size(139, 51);
+            this.btnExtrato.TabIndex = 3;
+            this.btnExtrato.Text = "Ver extrato";
+            this.btnExtrato.UseVisualStyleBackColor = false;
+            this.btnExtrato.Click += new System.EventHandler(this.btnExtrato_Click);
+            // 
+            // lblSaldo
+            // 
+            this.lblSaldo.AutoSize = true;
+            this.lblSaldo.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaldo.Location = new System.Drawing.Point(57, 120);
+            this.lblSaldo.Name = "lblSaldo";
+            this.lblSaldo.Size = new System.Drawing.Size(66, 31);
+            this.lblSaldo.TabIndex = 4;
+            this.lblSaldo.Text = "R$ 0";
+            this.lblSaldo.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(500, 450);
+            this.Controls.Add(this.btnExtrato);
+            this.Controls.Add(this.lblSaldo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblOperacao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Button btnDepositar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnExtrato;
+        private System.Windows.Forms.Label lblSaldo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04AppBanco
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void txtValor_TextChanged(object sender, EventArgs e)
+        {
+
+            
+
+
+
+
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            // Para salvar o valor do saldo digitado//
+            double valor;
+            double saldo;
+
+            valor = double.Parse(txtValor.Text);
+            string novo_saldo = lblSaldo.Text;
+            // Replace serve para substituir algo//
+            novo_saldo = novo_saldo.Replace("R$", "");
+
+            saldo = double.Parse(novo_saldo);
+
+            
+            double soma;
+            if( lblOperacao.Text == "DEPÓSITO")
+            {
+               
+
+                soma = saldo + valor;
+
+            }
+            else
+            {
+
+
+                soma = valor - saldo;
+
+            }
+
+
+            
+
+            lblSaldo.Text = $"R$ {soma} ";
+
+
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            //Tirar a cor do depositar//
+            btnDepositar.BackColor = Color.Gray;
+            btnDepositar.ForeColor = Color.White;
+
+            // Pintar botão sacar//
+            btnSacar.BackColor = Color.Black;
+            btnSacar.ForeColor = Color.White;
+            lblOperacao.Text  = "SAQUE";
+
+
+
+
+
+
+
+
+            //ForeColor mudar cor da fonte
+        }
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+
+            
+
+            //Tirar a cor do sacar//
+            btnSacar.BackColor = Color.Gray;
+            btnSacar.ForeColor = Color.White;
+
+            // Pintar botão depositar//
+            btnDepositar.BackColor = Color.Black;
+            btnDepositar.ForeColor = Color.White;
+            lblOperacao.Text = "DEPÓSITO";
+        }
+
+        private void btnExtrato_Click(object sender, EventArgs e)
+        {
+            // Abrir nova tela quando clicar em extrato//
+            Form2 telaextrato = new Form2();
+            telaextrato.Show();
+        }
+    }
+}
